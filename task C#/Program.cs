32b2a9e7e376215f257776f2b8fad7c0419eaabd
«Alexander» <semeon.gromow@yandex.ru>
2022-12-26 23:38:41 +0300

    Третий коммит программа на языке С#

---
+++ task C#/Program.cs
@@ -0,0 +1,33 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+using System;
+
+namespace ConsoleFinalWork
+{
+    class Program
+    {
+        static void Sort(string[] arr1, string[] arr2)
+        {
+            int count = 0;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i].Length <= 3)
+                {
+                    arr2[count] = arr1[i];
+                    count++;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] arr1 = new string[4] { "hello", "2", "world", ":-)" };
+            string[] arr2 = new string[arr1.Length];
+            Sort(arr1, arr2);
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.Write($"{arr2[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
